6e026cb13a00c0db7c463bee491c0ce36b96fe7b
Quynh Nguyen <qtn2@case.edu>
2023-10-21 15:26:07 -0400

    button code

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,15 @@
 import Button from "../components/Button";
+import {HiArrowLongDown} from "react-icons/hi2";
+import RotateScroll from '../assets/rotatescroll.svg'
 const Home = () => {
     return (
         <div className="homepage"> 
             <div className="homepage-headliner"> 
                 <h1 className="headliner-content"> GAUSSISCAPE </h1>
+                <div>
+                    <img src={RotateScroll} alt="rotate to scroll" className="rotate-scroll"/>
+                    {/* <HiArrowLongDown size={25} className="rotate-scroll-icon"/> */}
+                </div>
             </div>
             <div className="home-buttons">
                 <Button to = "/mainpage"> START </Button>
